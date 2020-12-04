8fe5e38a3b3e93df089135fc0a36fcd4bf8348e7
Victor Betini <betinivictor@gmail.com>
2020-12-04 13:58:46 -0300

    adding files

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+function Home() {
+    return(
+        <div className="main-content">
+            <h1 className="logo">
+                Victor Betini
+            </h1>
+            <p className="description">Full Stack PHP Developer</p>
+        </div>
+    );
+}
+
+export default Home;
